c9278a92946c15d08724daab05d216ceb97546c1
EH <43977335+EthanHaque@users.noreply.github.com>
2020-03-15 21:54:06 -0500

    merging

---
+++ refactoring/manipulateImages.py
@@ -0,0 +1,10 @@
+from manipulatePixels import ManipulatePixels
+from PIL import Image
+
+class manipulateImages:
+
+    def __init__(self):
+        pass
+
+    def combineSigBits(self, path1, path2):
+        
--- refactoring/manipulatePixels.py
+++ refactoring/manipulatePixels.py
@@ -5,17 +5,46 @@ class ManipulatePixels:
     def __init__(self):
         pass
 
-    def binarize3D(self, array3D, rows, cols, channels):
+    def binarize3DDigits(self, array3D):
         binArray = []
-        for y in range(cols):
+        for i in range(2):
             binArray.append([])
-            for x in range(rows):
-                binArray.append(())
-                for z in range(channels):
-                    decimal = array3D[x][y][z]
-                    binaryNum = bin(decimal)[2:]
+            for y in range(len(array3D)):
+                binArray[i].append([])
+                for x in range(len(array3D[0])):
+                    binArray[i][y].append(())
+                    for z in range(len(array3D[0][0])):
+                        decimal = array3D[y][x][z]
+                        binaryNum = bin(decimal)[2:]
+                        paddedBinaryNum = (8 - len(binaryNum)) * "0" + binaryNum
+                        if(i == 0):
+                            binArray[i][y][x] += (paddedBinaryNum[:4],)
+                        else:
+                            binArray[i][y][x] += (paddedBinaryNum[4:],)
+        return(binArray)
 
+    def binarizePixelArray(self, array3D):
+        sigInsigPixelArray = self.binarize3DDigits(array3D)
+        combinedPixelArray = []
+        for y in range(len(sigInsigPixelArray[0])):
+            combinedPixelArray.append([])
+            for x in range(len(sigInsigPixelArray[0][0])):
+                combinedPixelArray[y].append(())
+                for z in range(len(sigInsigPixelArray[0][0][0])):
+                        combinedPixelArray[y][x] += (sigInsigPixelArray[0][y][x][z] + sigInsigPixelArray[1][y][x][z], )
+        return(combinedPixelArray)
+
+    def getSigDigs(self, array3D):
+        sigInsigPixelArray = self.binarize3DDigits(array3D)
+        return(sigInsigPixelArray[0])
+
+    def getInsigDigs(self, array3D):
+        sigInsigPixelArray = self.binarize3DDigits(array3D)
+        return(sigInsigPixelArray[1])
+                    
 
 if __name__ == "__main__":
+    image = r"C:\Users\ebaba\Desktop\Python\Image_Steganography\images\stitch.png"
+    readImage = ReadImage(image)
     manipulatePixels = ManipulatePixels()
-    manipulatePixels.binarize3D([], 10, 10, 3)
+    print(manipulatePixels.binarizePixelArray(readImage.getPixelArray()))
