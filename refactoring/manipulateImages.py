dcda8f465346cb81aae19ecdba77dd2d85da9463
EH <43977335+EthanHaque@users.noreply.github.com>
2020-03-16 01:08:27 -0500

    works

--- refactoring/manipulateImages.py
+++ refactoring/manipulateImages.py
@@ -1,10 +1,61 @@
 from manipulatePixels import ManipulatePixels
+from readImage import ReadImage
 from PIL import Image
 
-class manipulateImages:
+class ManipulateImages:
 
     def __init__(self):
         pass
 
     def combineSigBits(self, path1, path2):
+        manipulatePixels = ManipulatePixels()
+        mainImage = ReadImage(path1)
+        hiddenImage = ReadImage(path2)
+        mainImageSigBits = manipulatePixels.getSigDigs(mainImage.getPixelArray())
+        hiddenImageSigBits = manipulatePixels.getSigDigs(hiddenImage.getPixelArray())
+        return(manipulatePixels.combinePixelArray(mainImageSigBits, hiddenImageSigBits))
+
+    def combineImages(self, path1, path2, name):
+        manipulatePixels = ManipulatePixels()
+        binArray3D = self.combineSigBits(path1, path2)
+        array3D = manipulatePixels.decimalizePixelArray(binArray3D)
+        im = Image.new(mode = "RGB", size = (len(array3D[0]), len(array3D)))
+        for y in range(len(array3D)):
+            for x in range(len(array3D[0])):
+                im.putpixel((x,y), array3D[y][x])
+        im.save(name)
+
+    def seperateSigBits(self, path):
+        manipulatePixels = ManipulatePixels()
+        image = ReadImage(path)
+        bits = manipulatePixels.binarize3DDigits(image.getPixelArray())
+        paddedBits = manipulatePixels.pad4DSigBitArray(bits)
+        return(paddedBits)
+
+    def seperateImages(self, path, hiddenName, mainName):
+        manipulatePixles = ManipulatePixels()
+        binArray4D = self.seperateSigBits(path)
+        mainImageBin3DArray = binArray4D[1]
+        hiddenImageBin3DArray = binArray4D[0]
+        mainImage3DArray = manipulatePixles.decimalizePixelArray(mainImageBin3DArray)
+        hiddenImage3DArray = manipulatePixles.decimalizePixelArray(hiddenImageBin3DArray)
+        mainIm = Image.new(mode = "RGB", size = (len(mainImage3DArray[0]), len(mainImage3DArray)))
+        for y in range(len(mainImage3DArray)):
+            for x in range(len(mainImage3DArray[0])):
+                mainIm.putpixel((x,y), mainImage3DArray[y][x])
+        mainIm.save(mainName)
+        hiddenIm = Image.new(mode = "RGB", size = (len(hiddenImage3DArray[0]), len(hiddenImage3DArray)))
+        for y in range(len(hiddenImage3DArray)):
+            for x in range(len(hiddenImage3DArray[0])):
+                hiddenIm.putpixel((x,y), hiddenImage3DArray[y][x])
+        hiddenIm.save(hiddenName)
         
+        
+
+if __name__ == "__main__":
+    path1 = r"C:\Users\ebaba\Desktop\Python\Image_Steganography\images\cat.png"
+    path2 = r"C:\Users\ebaba\Desktop\Python\Image_Steganography\images\poem.png"
+    path = r"C:\Users\ebaba\Desktop\Python\Image_Steganography\test.png"
+    manipulateImages = ManipulateImages()
+    manipulateImages.combineImages(path1, path2, "test.png")
+    #manipulateImages.seperateImages(path, "main.png", "hidden.png")
--- refactoring/manipulatePixels.py
+++ refactoring/manipulatePixels.py
@@ -34,6 +34,52 @@ class ManipulatePixels:
                         combinedPixelArray[y][x] += (sigInsigPixelArray[0][y][x][z] + sigInsigPixelArray[1][y][x][z], )
         return(combinedPixelArray)
 
+    def pad4DSigBitArray(self, array4D):
+        paddedArray = []
+        for i in range(2):
+            paddedArray.append([])
+            for y in range(len(array4D[0])):
+                paddedArray[i].append([])
+                for x in range(len(array4D[0][0])):
+                    paddedArray[i][y].append(())
+                    for z in range(len(array4D[0][0][0])):
+                        paddedArray[i][y][x] += (array4D[i][y][x][z] + "0" * (8 - len(array4D[i][y][x][z])), )
+        return(paddedArray)
+
+    def decimalizePixelArray(self, array3D):
+        decimalPixelArray = []
+        for y in range(len(array3D)):
+            decimalPixelArray.append([])
+            for x in range(len(array3D[0])):
+                decimalPixelArray[y].append(())
+                for z in range(len(array3D[0][0])):
+                    decimalPixelArray[y][x] += (int(array3D[y][x][z], 2), )
+        return(decimalPixelArray)
+
+    def addNullChannel(self, array3D):
+        addedChannel = []
+        for y in range(len(array3D)):
+            addedChannel.append([])
+            for x in range(len(array3D[0])):
+                addedChannel[y].append(array3D[y][x] + ("0", ))
+        return(addedChannel)
+                
+    def combinePixelArray(self, array3D1, array3D2):
+        if(len(array3D1[0][0]) > len(array3D2[0][0])):
+            for i in range(len(array3D1[0][0]) - len(array3D2[0][0])):
+                array3D2 = self.addNullChannel(array3D2)
+        elif(len(array3D2[0][0]) > len(array3D1[0][0])):
+            for i in range(len(array3D2[0][0]) - len(array3D1[0][0])):
+                array3D1 = self.addNullChannel(array3D1)
+        combinedPixelArray = []
+        for y in range(len(array3D1)):
+            combinedPixelArray.append([])
+            for x in range(len(array3D1[0])):
+                combinedPixelArray[y].append(())
+                for z in range(len(array3D1[0][0])):
+                    combinedPixelArray[y][x] += (array3D1[y][x][z] + array3D2[y][x][z],)
+        return(combinedPixelArray)
+        
     def getSigDigs(self, array3D):
         sigInsigPixelArray = self.binarize3DDigits(array3D)
         return(sigInsigPixelArray[0])
@@ -47,4 +93,5 @@ if __name__ == "__main__":
     image = r"C:\Users\ebaba\Desktop\Python\Image_Steganography\images\stitch.png"
     readImage = ReadImage(image)
     manipulatePixels = ManipulatePixels()
-    print(manipulatePixels.binarizePixelArray(readImage.getPixelArray()))
+    arr = manipulatePixels.binarizePixelArray(readImage.getPixelArray())
+    print(manipulatePixels.addNullChannel(arr))
